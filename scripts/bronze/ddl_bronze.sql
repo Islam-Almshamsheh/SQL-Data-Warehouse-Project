f50eabfc5312013c21fcaee70e5f1786467edce4
Islam-Almshamsheh <148493833+Islam-Almshamsheh@users.noreply.github.com>
2025-09-22 14:54:46 +0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -65,9 +65,9 @@ CREATE TABLE bronze.erp_loc_a101(
 );
 GO
 
-IF OBJECT_ID ('bronze.px_cat_g1v2','U') IS NOT NULL
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2','U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
-CREATE TABLE bronze.px_cat_g1v2(
+CREATE TABLE bronze.erp_px_cat_g1v2(
 	id VARCHAR(50),
 	cat VARCHAR(50),
 	subcat VARCHAR(50),
